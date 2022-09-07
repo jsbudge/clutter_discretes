Changed the exoConfig a bit. Also tweaked the starting covariances and modified the adaptive covariance (it's still broken, though). Fixed a bug in the process function and the displaying of target coordinates.

main.py
@@ -49,7 +49,7 @@ gimbal_fnme = [
 csv_fnme = './test.csv'
 do_stanag = False
 do_video = True
-max_cpi_count = 500
+max_cpi_count = 100
 
 # Other settings are contained in the XML file in this same directory, grab them
 config = ExoConfiguration()
@@ -107,7 +107,7 @@ noisePowerVarLPFs = []
 
 # Initialize the tracker
 # Measurement Noise Covariance
-meas_cov = np.diag(np.array([2., 2., 10, 10, 10, 10, 10, 10, 5, 5])**2)
+meas_cov = np.diag(np.array([2., 3.5, .2, .2, .2, .2, .2, .2, .5, .5])**2)
 tracker = TrackManager(deadtrack_time=2., R=meas_cov)
 
 # Chirp and matched filter calculations
@@ -129,6 +129,7 @@ slowTimeWithChannels[1, ...] = slowTimeWindow.T
 slow_time_gpu = cupy.array(slowTimeWithChannels, dtype=np.complex128)
 fig = None
 plotdata = []
+cpi_times = []
 
 with open(csv_fnme, 'w') as csv:
     idx_t = np.arange(nframes)
@@ -280,7 +281,7 @@ with open(csv_fnme, 'w') as csv:
 
         """ Segmentation |||||||||||||||||||||||||||||||||||||||||||||||||||||||"""
         # if cpi_count % 2 == 0:
-        tracker.propogate(timeSinceValidCPI)
+        tracker.propogate(ts[0])
         targetList = getExoClutterDetectedMoversRVBlob(
             detMap, antPos, boresightVec.flatten(), myRanges, myDopps, antVel, sdr_f[0].fc,
             rp.origin, antAz)
@@ -288,11 +289,12 @@ with open(csv_fnme, 'w') as csv:
         # Add the detections we got to the dwell track manager
         valid_cpi_time = timeSinceValidCPI if timeSinceValidCPI > 0 else cpi_time
         for t in targetList:
-            tracker.add(t, ts[0], boresightVec.flatten(), rp.origin, sdr_f[0].fc, threshold=20)
+            tracker.add(t, ts[0], boresightVec.flatten(), rp.origin, sdr_f[0].fc, dt=valid_cpi_time, threshold=10)
+
+        cpi_times.append(ts[0])
 
         tracker.cullTracks(ts[0])
         tracker.fuse()
-        # tracker.propogate(timeSinceValidCPI if timeSinceValidCPI > 0 else cpi_time)
 
         """ Parameter Estimation |||||||||||||||||||||||||||||||||||||||||||||||"""
         # First, compute the hAgl (to get closer to the actual hAgl, we should
@@ -460,6 +462,7 @@ with open(csv_fnme, 'w') as csv:
                     csvWriter.writerow(cpiData)
                     # break
         if cpi_count >= max_cpi_count:
+            tracker.removeSingletons()
             break
 del slow_time_gpu
 del mfilt_gpu
@@ -477,18 +480,16 @@ imageDat = ax.imshow(
 targ_rvel_array = [[] for _ in range(cpi_count)]
 for tidx, t in enumerate(tracker.tracks):
     targ_log = t.state_log[:, :8]
-    targ_rvel = np.linalg.norm(targ_log[:, 5:], axis=1)
-    for cpi in range(len(plotdata)):
+    for cidx, cpi in enumerate(cpi_times):
         if cpi in tracker.update_times[tidx]:
             nt = [idx for idx in range(len(tracker.update_times[tidx])) if cpi == tracker.update_times[tidx][idx]][0]
-            targ_rvel_array[cpi].append([targ_rvel[nt], targ_log[nt, 1]])
+            targ_rvel_array[cidx].append([targ_log[nt, 0], targ_log[nt, 1]])
 for tidx, t in enumerate(tracker.deadtracks):
     targ_log = t.state_log[:, :8]
-    targ_rvel = np.linalg.norm(targ_log[:, 5:], axis=1)
-    for cpi in range(len(plotdata)):
+    for cidx, cpi in enumerate(cpi_times):
         if cpi in tracker.dead_updates[tidx]:
             nt = [idx for idx in range(len(tracker.dead_updates[tidx])) if cpi == tracker.dead_updates[tidx][idx]][0]
-            targ_rvel_array[cpi].append([targ_rvel[nt], targ_log[nt, 1]])
+            targ_rvel_array[cidx].append([targ_log[nt, 0], targ_log[nt, 1]])
 for n in range(cpi_count):
     if len(targ_rvel_array[n]) != 0:
         targ_rvel_array[n] = np.array(targ_rvel_array[n])
@@ -536,14 +537,14 @@ anim = animation.FuncAnimation(
 # Save the animation as mp4 video file
 anim.save('./Test_TruthRDVideo.mp4')
 
-tr1 = tracker.tracks[0].state_log[:, :8]
+tr1 = tracker.tracks[0].state_log[:, :14]
 mx, my = np.meshgrid(np.linspace(-100, 500, 30), np.linspace(1300, 2800, 30))
 mlat, mlon, malt = enu2llh(mx.ravel(), my.ravel(), np.zeros((mx.shape[0] * mx.shape[1],)), rp.origin)
 malt = getElevationMap(mlat, mlon)
 me, mn, mu = llh2enu(mlat, mlon, malt, rp.origin)
-fig = px.scatter_3d(x=tr1[:, 0], y=tr1[:, 1], z=tr1[:, 2])
-# fig.add_mesh3d(x=me, y=mn, z=mu)
+fig = px.scatter_3d(x=tr1[:, 11], y=tr1[:, 12], z=tr1[:, 13])
+fig.add_mesh3d(x=me, y=mn, z=mu)
 for tr in tracker.tracks:
-    tr1 = tr.state_log[:, :8]
-    fig.add_scatter3d(x=tr1[:, 0], y=tr1[:, 1], z=tr1[:, 2])
+    tr1 = tr.state_log[:, :14]
+    fig.add_scatter3d(x=tr1[:, 11], y=tr1[:, 12], z=tr1[:, 13])
 fig.show()

targetlib.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import *
 from dataclasses import dataclass
 from simulation_functions import getElevation, enu2llh
 from scipy.spatial.distance import pdist
@@ -43,12 +44,13 @@ class Target(object):
 
 class Track:
 
-    def __init__(self, fd_o, r_o, ant_pos, ant_vel, boresight, origin, fc, Q=None, R=None):
+    def __init__(self, fd_o, r_o, ant_pos, ant_vel, boresight, origin, fc, init_time, dt=1.0, Q=None, R=None):
         targ_pos = calcGroundENU(r_o, ant_pos, boresight, origin)
         targ_vel = calcDoppVel(boresight, fd_o, fc, ant_vel)
         init_state = np.array([fd_o, r_o, *ant_pos, *ant_vel, 0, 0, 0, *targ_pos, *targ_vel,
                                np.arctan2(boresight[0], boresight[1]), 0, np.arcsin(boresight[2]), 0])
-        self._kf = UKF(init_state, process, measure, Q=Q, R=R)
+        self.origin = origin
+        self._kf = UKF(init_state, self.process, self.measure, init_time, dt=dt, adaptive_constant=None, Q=Q, R=R)
 
     def accept(self, target):
         Vi = np.linalg.pinv(self._kf.R)
@@ -64,8 +66,8 @@ class Track:
     def update(self, z, ts):
         self._kf.update(z, ts)
 
-    def move(self, ts=0):
-        self._kf.predict()
+    def move(self, ts=None):
+        self._kf.predict(ts)
 
     def merge(self, ot):
         pass
@@ -82,54 +84,163 @@ class Track:
     def state_log(self):
         return np.array(self._kf.x_log)
 
+    def process(self, x, dt=1.0):
+        new_state = np.zeros((len(x),))
+        new_plane_pos = x[2:5] + x[5:8] * dt
+        new_targ_pos = x[11:14] + x[14:17] * dt
+        rng = np.linalg.norm(new_plane_pos - new_targ_pos)
+        new_az = x[17] + x[18] * dt
+        new_el = x[19] + x[20] * dt
+        # Inertial grazing angle
+        boresight = np.array([np.cos(new_el) * np.sin(new_az),
+                  np.cos(new_el) * np.cos(new_az),
+                  -np.sin(new_el)])
+        # Radial velocity
+        new_state[0] = np.linalg.norm(x[5:8] - x[14:17]) * np.sign(np.cross(boresight, x[5:8]))[2]
+        # Range to target
+        new_state[1] = rng
+        # Plane pos
+        new_state[2:5] = new_plane_pos
+        # Plane vel
+        new_state[5:8] = x[5:8] + x[8:11] * dt
+        # Plane accel
+        new_state[8:11] = x[8:11]
+        # Target pos
+        new_state[11:14] = new_targ_pos
+        # Target vel
+        new_state[14:17] = x[14:17]
+        # Inertial Azimuth
+        new_state[17] = new_az
+        # Inertial Azimuth Velocity
+        new_state[18] = x[18]
+        # Intertial Grazing Angle
+        new_state[19] = new_el
+        # Inertial grazing angle velocity
+        new_state[20] = x[20]
+        return new_state
 
-def process(x, dt=1.0):
-    new_state = np.zeros((len(x),))
-    new_plane_pos = x[2:5] + x[5:8] * dt
-    new_targ_pos = x[8:11] + x[11:14] * dt
-    boresight = np.array([np.cos(x[19]) * np.sin(x[17]),
-                  np.cos(x[19]) * np.cos(x[17]),
-                  -np.sin(x[19])])
-    # Radial velocity
-    new_state[0] = np.linalg.norm(x[5:8] - x[11:14]) * np.sign(np.cross(boresight, x[5:8]))[2]
-    # Range to target
-    new_state[1] = np.linalg.norm(new_plane_pos - new_targ_pos)
-    # Plane pos
-    new_state[2:5] = new_plane_pos
-    # Plane vel
-    new_state[5:8] = x[5:8] + dt * x[8:11]
-    # Plane accel
-    new_state[8:11] = x[8:11]
-    # Target pos
-    new_state[11:14] = new_targ_pos
-    # Target vel
-    new_state[14:17] = x[14:17]
-    # Inertial Azimuth
-    new_state[17] = x[17] + dt * x[18]
-    # Inertial Azimuth Velocity
-    new_state[18] = new_state[18]
-    # Inertial Elevation
-    new_state[19] = x[19] + dt * x[20]
-    # Inertial Elevation Velocity
-    new_state[20] = x[20]
-    return new_state
+    def measure(self, state, dt=1.0):
+        new_meas = np.zeros((10,))
+        # Radial velocity
+        new_meas[0] = state[0]
+        # Range to target
+        new_meas[1] = state[1]
+        # Plane pos
+        new_meas[2:5] = state[2:5]
+        # Plane vel
+        new_meas[5:8] = state[5:8]
+        # Inertial Azimuth
+        new_meas[8] = state[17]
+        # Inertial grazing
+        new_meas[9] = state[19]
+        return new_meas
 
 
-def measure(state, dt=1.0):
-    new_meas = np.zeros((10,))
-    # Radial velocity
-    new_meas[0] = state[0]
-    # Range to target
-    new_meas[1] = state[1]
-    # Plane pos
-    new_meas[2:5] = state[2:5]
-    # Plane vel
-    new_meas[5:8] = state[5:8]
-    # Inertial Azimuth
-    new_meas[8] = state[17]
-    # Inertial Elevation
-    new_meas[9] = state[19]
-    return new_meas
+class TrackManager(object):
+    _tracks = None
+
+    def __init__(self, deadtrack_time=5, Q=None, R=None):
+        self._tracks = []
+        self._deadtracks = []
+        self._dt = deadtrack_time
+        self.update_times = []
+        self.dead_updates = []
+        if R is not None:
+            self._errs = R
+        else:
+            self._errs = np.diag(np.array([2.5, 10, 10, 10, 10, 10., 10, 10, 10])**2)
+        self._R = R
+        self._Q = Q
+
+    def add(self, t, current_time, boresight, origin, fc, dt=1.0, threshold=10):
+        if len(self._tracks) > 1:
+            # Calculate Mahal distance between t and the tracks
+            Vi = np.linalg.pinv(self._errs)
+            pv = self.getTrackPosVel()
+            mu = t.meas
+            dists = np.array([np.sqrt((pv[n, :] - mu).dot(Vi.dot(pv[n, :] - mu)))
+                              for n in range(len(self._tracks))])
+            if np.any(dists < threshold):
+                trs = np.argsort(dists)
+                tr = 0
+                while current_time in self._tracks[trs[tr]]._kf.update_time:
+                    tr += 1
+                    if dists[trs[tr]] > threshold:
+                        nt = Track(t.rad_vel, t.range, t.ant_pos, t.ant_vel, boresight, origin, fc, current_time,
+                                   dt=dt, Q=self._Q, R=self._R)
+                        self._tracks.append(nt)
+                        self.update_times.append([current_time])
+                        return
+                self._tracks[trs[tr]].update(mu, current_time)
+                if current_time not in self.update_times[trs[tr]]:
+                    self.update_times[trs[tr]].append(current_time)
+            else:
+                nt = Track(t.rad_vel, t.range, t.ant_pos, t.ant_vel, boresight, origin, fc, current_time,
+                           dt=dt, Q=self._Q, R=self._R)
+                self._tracks.append(nt)
+                self.update_times.append([current_time])
+        else:
+            nt = Track(t.rad_vel, t.range, t.ant_pos, t.ant_vel, boresight, origin, fc, current_time,
+                       dt=dt, Q=self._Q, R=self._R)
+            self._tracks.append(nt)
+            self.update_times.append([current_time])
+
+    def propogate(self, ts):
+        # For each of the tracks, update the position based on radial velocity
+        for tr in self._tracks:
+            tr.move(ts)
+
+    def fuse(self, threshold=10):
+        Vi = np.linalg.pinv(self._errs)
+        dists = pdist(self.getTrackPosVel(), metric='mahalanobis', VI=Vi)
+        fuzors = dists < threshold
+        if np.any(fuzors):
+            nuke = np.zeros((len(self._tracks,))).astype(bool)
+            idxes = np.array(list(combinations(np.arange(len(self._tracks)), 2)))[dists < threshold, :]
+            uniques = np.unique(idxes)
+            # Iterate until all the idxes are taken care of
+            while len(uniques) > 0:
+                val = uniques[0]
+                mergers = idxes[np.logical_or(idxes[:, 0] == val, idxes[:, 1] == val), :]
+                for n in range(1, mergers.shape[0]):
+                    deletor = mergers[n, 0] if mergers[n, 0] != val else mergers[n, 1]
+                    self._tracks[val].merge(self._tracks[deletor])
+                    nuke[deletor] = True
+                    idxes = idxes[np.logical_not(np.logical_or(idxes[:, 0] == deletor, idxes[:, 1] == deletor)), :]
+                idxes = idxes[np.logical_not(np.logical_or(idxes[:, 0] == val, idxes[:, 1] == val)), :]
+                uniques = np.unique(idxes)
+            # Remove all the merged tracks in reverse order so they don't mess each other up
+            for idx in sorted(np.arange(len(self._tracks))[nuke], reverse=True):
+                del self._tracks[idx]
+                del self.update_times[idx]
+
+    def cullTracks(self, ts):
+        nuke = [idx for idx in range(len(self._tracks)) if self.update_times[idx][-1] < ts - self._dt]
+        for idx in sorted(nuke, reverse=True):
+            # If it's not truly a track, just delete it
+            if len(self.update_times[idx]) < 2:
+                del self._tracks[idx]
+                del self.update_times[idx]
+            else:
+                self._deadtracks.append(self._tracks.pop(idx))
+                self.dead_updates.append(self.update_times.pop(idx))
+
+    def removeSingletons(self):
+        nuke = [idx for idx in range(len(self._tracks)) if len(self.update_times[idx]) < 2]
+        for idx in sorted(nuke, reverse=True):
+            del self._tracks[idx]
+            del self.update_times[idx]
+
+    def getTrackPosVel(self):
+        return np.array([t.meas for t in self._tracks])
+
+    @property
+    def tracks(self):
+        return self._tracks
+
+    @property
+    def deadtracks(self):
+        return self._deadtracks
 
 
 def calcGroundENU(rng, platform, boresight, origin):
@@ -178,88 +289,37 @@ def calcDoppVel(boresight, rad_vel, fc, platform_vel):
     return vr
 
 
-class TrackManager(object):
-    _tracks = None
+def computeGrazingAngle(effAzIR, grazeIR, antPos, theRange, origin):
+    # initialize the pointing vector to first range bin
+    Rvec = array([cos(grazeIR) * sin(effAzIR),
+                  cos(grazeIR) * cos(effAzIR),
+                  -sin(grazeIR)])
 
-    def __init__(self, deadtrack_time=5, Q=None, R=None):
-        self._tracks = []
-        self._deadtracks = []
-        self._dt = deadtrack_time
-        self.update_times = []
-        self.dead_updates = []
-        if R is not None:
-            self._errs = R
-        else:
-            self._errs = np.diag(np.array([2.5, 10, 10, 10, 10, 10., 10, 10, 10, 10])**2)
-        self._R = R
-        self._Q = Q
+    groundPoint = antPos + Rvec * theRange
+    nlat, nlon, alt = enu2llh(*groundPoint, origin)
+    # look up the height of the surface below the aircraft
+    surfaceHeight = getElevation((nlat, nlon), False)
+    # check the error in the elevation compared to what was calculated
+    elevDiff = surfaceHeight - alt
 
-    def add(self, t, current_time, boresight, origin, fc, threshold=10):
-        if len(self._tracks) > 1:
-            # Calculate Mahal distance between t and the tracks
-            Vi = np.linalg.pinv(self._errs)
-            pv = self.getTrackPosVel()
-            mu = t.meas
-            dists = np.array([np.sqrt((pv[n, :] - mu).dot(Vi.dot(pv[n, :] - mu)))
-                              for n in range(len(self._tracks)) if current_time not in self.update_times[n]])
-            if np.any(dists < threshold):
-                tr = np.where(dists == dists.min())[0][0]
-                self._tracks[tr].update(mu, current_time)
-                if current_time not in self.update_times[tr]:
-                    self.update_times[tr].append(current_time)
-            else:
-                nt = Track(t.rad_vel, t.range, t.ant_pos, t.ant_vel, boresight, origin, fc, Q=self._Q, R=self._R)
-                self._tracks.append(nt)
-                self.update_times.append([current_time])
-        else:
-            nt = Track(t.rad_vel, t.range, t.ant_pos, t.ant_vel, boresight, origin, fc, Q=self._Q, R=self._R)
-            self._tracks.append(nt)
-            self.update_times.append([current_time])
+    iterationThresh = 2
+    heightDiffThresh = 1.0
+    numIterations = 0
+    newGrazeR = grazeIR + 0.0
+    # iterate if the difference is greater than 1.0 m
+    while abs(elevDiff) > heightDiffThresh and numIterations < iterationThresh:
+        hAgl = antPos[2] + origin[2] - surfaceHeight
+        newGrazeR = arcsin(hAgl / theRange)
+        if isnan(newGrazeR) or isinf(newGrazeR):
+            print('NaN or inf found.')
+        Rvec = array([cos(newGrazeR) * sin(effAzIR),
+                      cos(newGrazeR) * cos(effAzIR),
+                      -sin(newGrazeR)])
+        groundPoint = antPos + Rvec * theRange
+        nlat, nlon, alt = enu2llh(*groundPoint, origin)
+        surfaceHeight = getElevation((nlat, nlon), False)
+        # check the error in the elevation compared to what was calculated
+        elevDiff = surfaceHeight - alt
+        numIterations += 1
 
-    def propogate(self, ts):
-        # For each of the tracks, update the position based on radial velocity
-        for tr in self._tracks:
-            tr.move(ts)
-
-    def fuse(self, threshold=10):
-        Vi = np.linalg.pinv(self._errs)
-        dists = pdist(self.getTrackPosVel(), metric='mahalanobis', VI=Vi)
-        fuzors = dists < threshold
-        if np.any(fuzors):
-            nuke = np.zeros((len(self._tracks,))).astype(bool)
-            idxes = np.array(list(combinations(np.arange(len(self._tracks)), 2)))[dists < threshold, :]
-            uniques = np.unique(idxes)
-            # Iterate until all the idxes are taken care of
-            while len(uniques) > 0:
-                val = uniques[0]
-                mergers = idxes[np.logical_or(idxes[:, 0] == val, idxes[:, 1] == val), :]
-                for n in range(1, mergers.shape[0]):
-                    deletor = mergers[n, 0] if mergers[n, 0] != val else mergers[n, 1]
-                    self._tracks[val].merge(self._tracks[deletor])
-                    nuke[deletor] = True
-                    idxes = idxes[np.logical_not(np.logical_or(idxes[:, 0] == deletor, idxes[:, 1] == deletor)), :]
-                idxes = idxes[np.logical_not(np.logical_or(idxes[:, 0] == val, idxes[:, 1] == val)), :]
-                uniques = np.unique(idxes)
-            # Remove all the merged tracks in reverse order so they don't mess each other up
-            for idx in sorted(np.arange(len(self._tracks))[nuke], reverse=True):
-                del self._tracks[idx]
-                del self.update_times[idx]
-
-    def cullTracks(self, ts):
-        nuke = [idx for idx in range(len(self._tracks)) if self.update_times[idx][-1] < ts - self._dt]
-        for idx in sorted(nuke, reverse=True):
-            self._deadtracks.append(self._tracks.pop(idx))
-            self.dead_updates.append(self.update_times.pop(idx))
-            # del self._tracks[idx]
-            # del self.update_times[idx]
-
-    def getTrackPosVel(self):
-        return np.array([t.meas for t in self._tracks])
-
-    @property
-    def tracks(self):
-        return self._tracks
-
-    @property
-    def deadtracks(self):
-        return self._deadtracks
+    return newGrazeR, Rvec, surfaceHeight, numIterations